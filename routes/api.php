8a178a39675c362b4c1290d4b47f9fa6d9e431cb
Ei Ei Lwin <eieilwin.dev@gmail.com>
2025-12-07 14:45:47 +0700

    add api version

--- routes/api.php
+++ routes/api.php
@@ -7,27 +7,21 @@ use App\Projects\Infrastructure\Http\Controllers\ProjectController;
 use App\Projects\Infrastructure\Http\Controllers\TaskController;
 use App\Users\Infrastructure\Http\Controllers\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/users', function () {
-    // Placeholder for user listing
-    $users = User::class::all();
-    return response()->json(['data' => $users]);
-});
+Route::prefix('v1')->group(function () {
+    
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function () {
-    return request()->user();
-});
+    Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::prefix('projects')->group(function () {
+        Route::post('/', [ProjectController::class, 'store']);
+        Route::get('/{id}', [ProjectController::class, 'show']);
+    });
 
-Route::middleware('auth:sanctum')->group(function () {
-  
-  Route::prefix('projects')->group(function () {
-      Route::post('/', [ProjectController::class, 'store']);
-      Route::get('/{id}', [ProjectController::class, 'show']);
-  });
+    Route::prefix('tasks')->group(function () { 
+        Route::post('/', [TaskController::class, 'store']);
+    });
 
-  Route::prefix('tasks')->group(function () { 
-      Route::post('/', [TaskController::class, 'store']);
-  });
-
+    });
 });
